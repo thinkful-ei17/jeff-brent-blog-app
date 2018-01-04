c7cdee076f7034b1e0bac360fea3a5c7bb011518
Chris Klanac <cklanac@users.noreply.github.com>
2018-01-04 12:56:21 -0600

    fix: typos bad copy/paste

--- config.js
+++ config.js
@@ -1,9 +1,5 @@
 'use strict';
 
-exports.PORT = process.env.PORT || 8080; 
-
-'use strict';
-
 const DATABASE_URL = process.env.DATABASE_URL || 'postgres://localhost/blog-app';
 
 exports.DATABASE = {
