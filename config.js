41af3041b99bccbbf590b0cccf145bd301567a4f
Chris Klanac <cklanac@users.noreply.github.com>
2018-01-04 11:20:36 -0600

    update config.js with knex/pg connection info

--- config.js
+++ config.js
@@ -1,3 +1,16 @@
 'use strict';
 
 exports.PORT = process.env.PORT || 8080; 
+
+'use strict';
+
+const DATABASE_URL = process.env.DATABASE_URL || 'postgres://localhost/blog-app';
+
+exports.DATABASE = {
+  client: 'pg',
+  connection: DATABASE_URL,
+  pool: { min: 0, max: 2}, // elephantSQL
+  debug: process.env.NODE_ENV === 'development' // resolves to true or false
+};
+
+exports.PORT = process.env.PORT || 8080; 
